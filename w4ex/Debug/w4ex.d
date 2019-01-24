d5c2ab8a257584e8fed55f4a905aa92da25dbba3
VidishaShah29 <vidishashah29@yahoo.com>
2019-01-24 16:35:26 +0530

    Add files via upload
    find 1! + 2! + 3! + ... + N! using reduce

---
+++ w4ex/Debug/w4ex.d
@@ -0,0 +1,8 @@
+w4ex.d: ../w4ex.c /usr/include/mpich/mpi.h /usr/include/mpich/mpio.h \
+ /usr/include/mpich/mpi.h
+
+/usr/include/mpich/mpi.h:
+
+/usr/include/mpich/mpio.h:
+
+/usr/include/mpich/mpi.h:
